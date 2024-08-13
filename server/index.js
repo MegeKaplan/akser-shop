871b75ca343654b0c40eafd986f8f27916e9123b
MegeKaplan <kaplanmehmetege@gmail.com>
2024-08-13 18:59:07 +0300

    feat: knex configuration completed and CRUD operations tested

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import knex from "knex";
+import knexConfig from "./config/db/knexfile.js";
 
 const config = dotenv.config();
 
@@ -16,6 +18,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Server is running..." });
 });
+
+const db = knex(knexConfig.development);
+
+const getAdmins = async () => {
+  try {
+    const admins = await db("users").select("*").where({ is_admin: true });
+    console.log(admins);
+  } catch (error) {
+    console.error("Error retrieving admins:", error);
+  } finally {
+    db.destroy();
+  }
+};
+
+getAdmins();
 
 app.listen(PORT, () => {
   console.log(`Server is running...`);
